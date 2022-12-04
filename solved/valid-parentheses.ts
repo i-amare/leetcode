b47396b80772404b2bddbcd638f58a8d690f8012
Nsovo Ngobeni <97952201+i-amare@users.noreply.github.com>
2022-12-04 22:02:19 +0200

    Create valid-parentheses.ts

---
+++ solved/valid-parentheses.ts
@@ -0,0 +1,23 @@
+/**
+ * The knows API is defined in the parent class Relation.
+ * isBadVersion(version: number): boolean {
+ *     ...
+ * };
+ */
+
+const solution = function (isBadVersion: any) {
+	return function (n: number): number {
+		let left = 0;
+		let right = n;
+
+		while (left < right) {
+			const mid = Math.floor((left + right) / 2);
+			if (isBadVersion(mid)) {
+				right = mid;
+			} else {
+				left = mid;
+			}
+			if (right - left <= 1) return isBadVersion(left) ? left : right;
+		}
+	};
+};
