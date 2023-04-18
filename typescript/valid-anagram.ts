8db6656dbb7498254dc9bdaae0f3ea113698fece
Nsovo Ngobeni <97952201+i-amare@users.noreply.github.com>
2023-04-18 16:20:23 +0200

    Update valid-anagram.ts

--- typescript/valid-anagram.ts
+++ typescript/valid-anagram.ts
@@ -2,15 +2,21 @@ function isAnagram(s: string, t: string): boolean {
 	const freq1 = new Map<string, number>();
 	const freq2 = new Map<string, number>();
 
-	for (let i = 0; i < s.length && i < t.length; i++) {
-			const l1 = s.charAt(i);
-			const l2 = t.charAt(i);
+	let anagram = true;
 
-			freq1[l1] = (freq1[l1] + 1) | 1;
-			freq2[l2] = (freq2[l2] + 1) | 1;
+	for (let i = 0; i < s.length || i < t.length; i++) {
+		const l1 = s.charAt(i);
+		const l2 = t.charAt(i);
+
+		freq1[l1] = (freq1[l1] + 1) | 1;
+		freq2[l2] = (freq2[l2] + 1) | 1;
 	}
 
-	freq1.keys
+	if (Object.keys(freq1).length != Object.keys(freq2).length) anagram = false;
 
-	return true
-};
+	[...Object.entries(freq1)].forEach((e: [string, number]) => {
+		if (freq2[e[0]] != e[1]) anagram = false;
+	});
+
+	return anagram;
+}
