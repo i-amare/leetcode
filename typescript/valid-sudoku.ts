133a299f32d2ef2274eb585b5c0199acc0e1ef22
Nsovo Ngobeni <97952201+i-amare@users.noreply.github.com>
2023-04-19 13:48:23 +0200

    Create valid-sudoku.ts
    Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
    
    Each row must contain the digits 1-9 without repetition.
    Each column must contain the digits 1-9 without repetition.
    Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
    Note:
    
    A Sudoku board (partially filled) could be valid but is not necessarily solvable.
    Only the filled cells need to be validated according to the mentioned rules.

---
+++ typescript/valid-sudoku.ts
@@ -0,0 +1,39 @@
+function isValidSudoku(board: string[][]): boolean {
+	// Check for duplicates across rows
+	for (const arr of board) {
+			if (hasDuplicates(arr)) return false;
+	}
+
+	// Check for duplicates across columns
+	for (let i = 0; i < board.length; i++) {
+			const arr = new Array<string>();
+			for (let j = 0; j < board.length; j++) {
+					arr.push(board[i][j]);
+			}
+			if (hasDuplicates(arr)) return false;
+	}
+
+	// Check for duplicates in grids
+	for (let i = 0; i < 3; i++) {
+			const arr = new Array<string>();
+			for (let j = 0; j < 3; j++) {
+					arr.push(...board[j].slice(0, 3));
+			}
+			if (hasDuplicates(arr)) return false;
+	}
+
+	return true;
+};
+
+// Check for any duplicates in an array
+function hasDuplicates(arr: string[]): boolean {
+	const map = new Map<string, string>();
+	for (const num of arr) {
+			if (map.get(num) != undefined) return true;
+			map.set(num, "");
+	}
+	
+	return false
+}
+
+Number.
