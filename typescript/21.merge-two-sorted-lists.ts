1e9f9f105534699c68454cc3e16d379160650997
Nsovo Ngobeni <97952201+i-amare@users.noreply.github.com>
2023-07-28 12:13:00 +0200

    recursive solution

--- typescript/21.merge-two-sorted-lists.ts
+++ typescript/21.merge-two-sorted-lists.ts
@@ -21,23 +21,15 @@ function mergeTwoLists(
 	list1: ListNode | null,
 	list2: ListNode | null
 ): ListNode | null {
-	let res: ListNode | null = new ListNode();
-	let pointer: ListNode | null = res;
+	if (!list1) return list2;
+	if (!list2) return list1;
 
-	while (list1 && list2) {
-		if (list1.val <= list2.val) {
-			pointer.next = list1;
-			list1 = list1.next;
-		} else {
-			pointer.next = list2;
-			list2 = list2.next;
-		}
-		pointer = pointer.next;
+	if (list1.val <= list2.val) {
+		list1.next = mergeTwoLists(list1.next, list2);
+		return list1;
+	} else {
+		list2.next = mergeTwoLists(list2.next, list1);
+		return list2;
 	}
-
-	if (!list1) pointer.next = list2;
-	if (!list2) pointer.next = list1;
-
-	return res.next;
 }
 // @lc code=end
